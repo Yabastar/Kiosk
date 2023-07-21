1cbf398374441bf87f52f4aeb3a83219c2a37071
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-20 21:00:06 -0500

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,5 +1,5 @@
-local term = require("term")
 local event = require("event")
+local gpu = require("component").gpu
 
 local windowX, windowY = 5, 5 -- Initial window position
 local windowWidth, windowHeight = 20, 10 -- Window size
@@ -7,17 +7,15 @@ local dragging = false
 
 -- Function to draw the window
 function drawWindow()
-    local myWindow = window.create(term.current(), windowX, windowY, windowWidth, windowHeight, true)
-    myWindow.setBackgroundColor(colors.red)
-    myWindow.clear()
-    myWindow.setCursorPos(1, 1)
-    myWindow.setTextColor(colors.white)
-    myWindow.write("Testing my window!")
+    gpu.fill(windowX, windowY, windowWidth, windowHeight, " ") -- Clear the area of the window
+    gpu.setBackground(0xFF0000) -- Red background
+    gpu.setForeground(0xFFFFFF) -- White text color
+    gpu.set(windowX, windowY, "Testing my window!")
 end
 
 -- Function to handle mouse events
 function handleMouseEvent(_, _, x, y, button, _)
-    if button == 1 and x >= windowX and x <= windowX + windowWidth and y >= windowY and y <= windowY + windowHeight then
+    if button == 0 and x >= windowX and x <= windowX + windowWidth and y >= windowY and y <= windowY + windowHeight then
         dragging = true
         local prevX, prevY = x, y
         while dragging do
@@ -25,7 +23,6 @@ function handleMouseEvent(_, _, x, y, button, _)
             windowX = windowX + (curX - prevX)
             windowY = windowY + (curY - prevY)
             prevX, prevY = curX, curY
-            term.clear()
             drawWindow()
         end
     end
@@ -33,7 +30,6 @@ end
 
 -- Main function to run the example
 function main()
-    term.clear()
     drawWindow()
     while true do
         handleMouseEvent(event.pull())
